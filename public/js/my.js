e43d8be9e362fc7f55993fc07dfbb65a9a6b334d
kamilderkacz <kamil.derkacz@go2.pl>
2017-02-16 23:31:27 +0100

    Add sortable section feature - frontend part

--- public/js/my.js
+++ public/js/my.js
@@ -104,6 +104,19 @@ $(document).ready(function () {
     });
     
     
+    $('#sortable').sortable({
+    axis: 'y',
+    update: function (event, ui) {
+        var data = $(this).sortable('serialize');
+
+        // POST to server using $.post or $.ajax
+//        $.ajax({
+//            data: data,
+//            type: 'POST',
+//            url: '/your/url/here'
+//        });
+    }
+    });
     
 
 
